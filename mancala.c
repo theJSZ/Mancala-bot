71be6f4a50fcba8e7d7a15edeb335dab61cd12a1
Jussi Rantala <jussi.v.rantala@helsinki.fi>
2024-10-09 12:49:04 +0300

    first code

---
+++ mancala.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+    /*
+    The board:
+
+    13 12 11 10 09 08 07     player2
+       00 01 02 03 04 05 06  player1
+
+    13 and 06 are "banks", other indices are "holes"
+    */
+    uint8_t board[14] = {0};
+}
